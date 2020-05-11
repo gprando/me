5f92b5e587f6ec03f081c3e21929b6f73d046109
Gabriel Prando <gprando55@gmail.com>
2020-05-11 20:51:31 -0300

    end

--- src/components/header.js
+++ src/components/header.js
@@ -5,31 +5,24 @@ import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 import { Container, Content, Links } from './styles';
 
-import { RiLightbulbFlashLine } from 'react-icons/ri'
-import { WiMoonWaxingCrescent2 } from 'react-icons/wi'
-const Header = () => {
-  const estado = true;
-  const Botao = estado ? RiLightbulbFlashLine : WiMoonWaxingCrescent2
-
-  return (
-    <Container>
-      <Content>
-        <nav>
-          <AniLink paintDrip hex="#333" direction="left" duration={0.35} to="/">
-            Gabriel Prando
+const Header = () => (
+  <Container>
+    <Content>
+      <nav>
+        <AniLink paintDrip hex="#333" direction="left" duration={0.35} to="/">
+          Gabriel Prando
         </AniLink>
-        </nav>
-        <Links>
-          <AniLink swipe top="entry" to="/blog" entryOffset={300}>Blog</AniLink>
-          <AniLink cover bg="#333" duration={0.2} to="/curriculo">Curriculo</AniLink>
-          <AniLink fade to="/portfolio">Portifólio</AniLink>
-          <button><Botao /></button>
-        </Links>
+      </nav>
+      <Links>
+        <AniLink swipe top="entry" to="/blog" entryOffset={300}>Blog</AniLink>
+        <AniLink cover bg="#333" duration={0.2} to="/curriculo">Curriculo</AniLink>
+        <AniLink fade to="/portfolio">Portifólio</AniLink>
+      </Links>
 
-      </Content>
-    </Container>
-  )
-}
+    </Content>
+  </Container>
+)
+
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
